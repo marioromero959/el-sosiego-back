1f32882f0312920f971bed46ef10322996d9823b
marioromwb <marioromwb@hotmail.com>
2026-03-13 10:16:14 -0300

    fix build

--- src/api/booking-setting/controllers/booking-setting.ts
+++ src/api/booking-setting/controllers/booking-setting.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::booking-setting.booking-setting');
+export default factories.createCoreController('api::booking-setting.booking-setting' as any);
